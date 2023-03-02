f38047d5410fddf8bb45d002d6b578082f5d49b3
woocheolll <woocheol2868@gmail.com>
2023-03-02 19:32:47 +0900

    게시판 CRUD

---
+++ board/src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,68 @@
+package com.example.board.controller;
+
+import com.example.board.dto.BoardDTO;
+import com.example.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+    private final BoardService boardService;
+    @GetMapping("/create")
+    public String createForm(){
+        return "create";
+
+    }
+    @PostMapping("/create")
+    public String create(@ModelAttribute BoardDTO boardDTO) {
+        boardService.save(boardDTO);
+        return "index";
+    }
+    @GetMapping("")
+    public String findAll(Model model){
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+    }
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model)
+                          {
+        /*
+            해당 게시글의 조회수를 하나 올리고
+            게시글 데이터를 가져와서 detail.html에 출력
+         */
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "detail";
+    }
+    @GetMapping("/update/{id}")
+    public String updateForm(@PathVariable Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("boardUpdate", boardDTO);
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute BoardDTO boardDTO, Model model) {
+        BoardDTO board = boardService.update(boardDTO);
+        model.addAttribute("board", board);
+        return "detail";
+//        return "redirect:/board/" + boardDTO.getId();
+    }
+
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
+}
